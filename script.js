048aa1a29adb38b0da41e5408efa9932de1ad37b
Gurami Beridze <guramivarberidze@gmail.com>
2022-12-26 22:16:31 +0400

    ფიდბექი

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const userInfo = document.querySelector('.user-info');
 
 //*calling functions
 getUsersData();
-getId();
+// getId();
+//1) კარგია ეს თუდუესბი წამოღება მუშაობს, ფქუნციას სახელი გადაარქვი უფრო გასგები რომ იყოს getTodoByUserId დაარქვი,
+// 2) დეფოლტად არაფერს არ უნდა დაუძახო, როცა პირველად ჩაიტვრითება როგორც დემო აპლიკაციაშია ეგრე დაწერე: დავალების სიის წამოსაღებად დააჭირე იუზერს.
+// ანუ ჯერ იუზერს ირჩევ და მერე ეძახი getTodoByUserId
+
 getTodo(1); //* რომელ აიდისაც გადაცემ მოაქვს მისი ტასკები
 //*get all user from server
 function getUsersData() {
+    //API მისმართი ცალკე const ცვლადში შეინახე. https://dummyjson.com
     fetch('https://dummyjson.com/users')
         .then(response => response.json())
         .then(allUser => {
@@ -14,6 +19,8 @@ function getUsersData() {
         })
 }
 //*render user by name and last name
+// როცა იუზერებს დაარენდერებ, თან ქლიქ ივენთი მიაბი onclick="selectUser(user.id)" ცალკე ფუნქცია აღწერე selectUser რომელიც 
+// მონიშნულ იუზერს ლურჯ ბექგრაუნდს დაადებს ლისტში და getTodo-ს დაუძახებს შესაბამისი ID ით.
 function renderUser(allUser) {
     for (let user of allUser) {
         list.innerHTML += `
@@ -25,28 +32,32 @@ function renderUser(allUser) {
     }
 }
 //*get id
-function getId() {
-    fetch('https://dummyjson.com/users')
-        .then(response => response.json())
-        .then(allId => {
-            // for (let id of allId.users){
-            //     getTodo(id)
+// ეს ფუქნცია არ გჭრიდება.
+// function getId() {
+//     fetch('https://dummyjson.com/users')
+//         .then(response => response.json())
+//         .then(allId => {
+//             // for (let id of allId.users){
+//             //     getTodo(id)
 
-            // }
-        })
-}
+//             // }
+//         })
+// }
 //* get all todo's
 function getTodo(id) {
-    fetch(`https://dummyjson.com/todos/user/${id}`) 
+    fetch(`https://dummyjson.com/todos/user/${id}`)
         .then(response => response.json())
         .then(allTodo => {
             renderTodo(allTodo.todos)
         });
 }
 function renderTodo(allTodo) {
+    let todos = '';
+    // innerHtml-ი ციკლში რომ გიწერია ყოველ ციკლის შესვლაზე მთლიანი cotnent ის რე რენდერს იწვევს, 
+    // მაგიტომაც ჯერ სტრინგი შევაკვრევინე მთლიანად თუდუესბი და ციკლედან რომ გამოვა მერე მივანიჭე ერთხელ.
+    // renderUser  - ის შემთხვევაშიც ეგრე გაქვს და ასე გადააკეთე.
     for (let todo of allTodo) {
-
-        content.innerHTML += `
+        todos += `
             <form class='todo-form'>
             <input type="checkbox" name="" id="">
             <label class='todo-txt'>${todo.todo}</label>
@@ -57,4 +68,7 @@ function renderTodo(allTodo) {
             </form>
             `
     }
+    // content გაგიმართლა რომ ქრომს შეუძლია html თეგში აიდი რაც გაქვს მაგას პირდაპირ მიწვდეს getElementById -ის გარეშე.
+    // სხვა ბრაუზერებში გაგისხავს, მაგიტომაც ელემენტი მოასელექთე და შეინახე ცვლადში.
+    content.innerHTML = todos;
 }
